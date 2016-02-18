631f78501c8deeed609e6e5c2f9d625b82ba8809
ismaeljay-x0223402 <ismaeljaycarpio1992@gmail.com>
2016-02-18 17:58:12 +0800

    commit on db backup

---
+++ AMS/MasterConfig/BackupDatabase.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AMS.MasterConfig
+{
+    public partial class BackupDatabase : System.Web.UI.Page
+    {
+        DAL.Filler foo = new DAL.Filler();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnBackup_Click(object sender, EventArgs e)
+        {
+            foo.Databasebackup();
+        }
+    }
+}
