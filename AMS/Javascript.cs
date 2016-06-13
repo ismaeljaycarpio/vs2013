f841a326c7021f439b164d892c21995bcc4caeb6
x0223402 - new laptop <ismaeljaycarpio1992@gmail.com>
2016-06-13 15:39:56 +0800

    added bs modal js

---
+++ AMS/Javascript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace AMS
+{
+    public class Javascript
+    {
+        public static void ShowModal(Control control, Page pageObject, string modalId)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.Append(@"<script type='text/javascript'>");
+            sb.Append("$('#" + modalId + "').modal('show');");
+            sb.Append(@"</script>");
+            ScriptManager.RegisterClientScriptBlock(control, pageObject.GetType(), Guid.NewGuid().ToString(), sb.ToString(), false);
+            //pageObject.ClientScript.RegisterClientScriptBlock(pageObject.GetType(), Guid.NewGuid().ToString(), sb.ToString(), false);
+        }
+
+        public static void HideModal(Control control, Page pageObject, string modalId)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.Append(@"<script type='text/javascript'>");
+            sb.Append("$('#" + modalId + "').modal('hide');");
+            sb.Append(@"</script>");
+            ScriptManager.RegisterClientScriptBlock(control, pageObject.GetType(), Guid.NewGuid().ToString(), sb.ToString(), false);
+            //pageObject.ClientScript.RegisterClientScriptBlock(pageObject.GetType(), Guid.NewGuid().ToString(), sb.ToString(), false);
+        }
+    }
+}
